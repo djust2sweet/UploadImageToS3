6005003b9756ea6fefe4690a27d14f025c7f0f28
djust2sweet <dominiquesweeting13@gmail.com>
2026-02-09 17:45:43 -0500

    Update API endpoint in HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://fzbdltvmp7.execute-api.us-east-1.amazonaws.com/default/s3image'
+const API_ENDPOINT = 'https://tkbo1dtl14.execute-api.us-east-2.amazonaws.com/default/DemoFunctionServerless'
 
 export default {
   name: 'app',
